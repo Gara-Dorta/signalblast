58e7a16ad1418a688a93609c56254915a81ee001
Era Dorta <era.dorta@proton.me>
2026-04-05 03:03:01 +0200

    fix: avoid manually verifying new identities
    If a user changed the phone number or reinstalled signal, just trust the new device without manual verification.

--- docker/run_signal_cli_rest_api.sh
+++ docker/run_signal_cli_rest_api.sh
@@ -3,4 +3,5 @@
 docker run --rm -p 8080:8080 \
     -v $HOME/.local/share/signal-api:/home/.local/share/signal-cli \
     -e MODE='json-rpc' \
+    -e JSON_RPC_TRUST_NEW_IDENTITIES='always' \
     bbernhard/signal-cli-rest-api:0.98

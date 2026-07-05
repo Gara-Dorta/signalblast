62701b7a46626f1a106f5b231f8ea2cc57e10cbf
Gara Dorta <1507774+Gara-Dorta@users.noreply.github.com>
2026-07-05 15:47:05 +0200

    fix: use latest signal-cli-rest-api image

--- docker/run_signal_cli_rest_api.sh
+++ docker/run_signal_cli_rest_api.sh
@@ -4,4 +4,4 @@ docker run --rm -p 8080:8080 \
     -v $HOME/.local/share/signal-api:/home/.local/share/signal-cli \
     -e MODE='json-rpc' \
     -e JSON_RPC_TRUST_NEW_IDENTITIES='always' \
-    bbernhard/signal-cli-rest-api:0.98
+    bbernhard/signal-cli-rest-api:latest
